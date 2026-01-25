3946511ee46b85ddcefeba5a860e9a8cad91afc8
Jakub Koláček <jakub.kolacek@ibacz.eu>
2026-01-25 10:31:29 +0100

    Add Astro sitemap and default robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import {defineConfig} from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+    site: 'https://kolacek.dev',
+    integrations: [tailwind(), sitemap()],
     i18n: {
         defaultLocale: 'en',
         locales: ['en', 'cs'],
